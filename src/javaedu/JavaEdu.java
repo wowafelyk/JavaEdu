67d0ff19c5dbfc4ec035a7705290900e02133b26
Volodymyr Felyk <wowa.feluk@gmail.com>
2015-04-06 07:50:48 +0300

    Create new program for tests. Threads. Moving ball. Signed-off-by: Volodymyr Felyk <wowa.feluk@gmail.com>

---
+++ src/javaedu/JavaEdu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javaedu;
+
+import javaedu.threads.bounce.Bounce;
+
+/**
+ *
+ * @author Fenix
+ */
+public class JavaEdu {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        String [] arr={"Start","project"};
+        Bounce.main(arr);
+    }
+    
+}
---
+++ src/javaedu/threads/bounce/BallComponent.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaedu.threads.bounce;
+
+/**
+ *
+ * @author Fenix
+ */
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+/**
+ * The component that draws the balls.
+ *
+ * @version 1.34 2012-01-26
+ * @author Cay Horstmann
+ */
+public class BallComponent extends JPanel {
+
+    private static final int DEFAULT_WIDTH = 450;
+    private static final int DEFAULT_HEIGHT = 350;
+
+    private java.util.List<Ball> balls = new ArrayList<>();
+
+    /**
+     * Add a ball to the component.
+     *
+     * @param b the ball to add
+     */
+    public void add(Ball b) {
+        balls.add(b);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g); // erase background
+        Graphics2D g2 = (Graphics2D) g;
+        for (Ball b : balls) {
+            g2.fill(b.getShape());
+        }
+    }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+}
